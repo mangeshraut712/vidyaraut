97364b58fa880d5376b62965b832657f98056866
vidyaraut <vidyaraut17297@gmail.com>
2025-12-08 14:34:31 +0530

    Fix ENVIRONMENT_FALLBACK error: add timeZone configuration to next-intl

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -11,6 +11,7 @@ export default getRequestConfig(async ({ locale }) => {
 
   return {
     locale: locale as string,
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages: (await import(`./messages/${locale}.json`)).default,
+    timeZone: 'Asia/Kolkata'
   };
 });
